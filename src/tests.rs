a268b506fad8c6fe751058d5196b784d2c4d67a8
Dmytro Ohorodnik <dmytroohorodnik@protonmail.com>
2023-05-04 20:56:07 -0700

    feat: add basic Lamport clock

---
+++ src/tests.rs
@@ -0,0 +1,10 @@
+use crate::*;
+
+#[test]
+fn test_clocks_are_updated_on_request() {
+    let mut node1 = Node::new(1);
+    let mut node2 = Node::new(2);
+    node1.send(&mut node2);
+    assert_eq!(3, node1.lamport_clock.latest_time);
+    assert_eq!(2, node2.lamport_clock.latest_time);
+}
